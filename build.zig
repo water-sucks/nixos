b2801312837efe9a8092ece3cc54c4f3b0eb0026
Varun Narravula <varun@snare.dev>
2023-06-26 11:20:38 -0700

    feat: first commit

---
+++ build.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "nixos",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    const run = b.addRunArtifact(exe);
+    run.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run.step);
+
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&exe_tests.step);
+}
---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "A unified NixOS tooling replacement for nixos-* utilities";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        zig = inputs.zig-overlay.packages.${system}."0.11.0";
+
+        inherit (inputs.gitignore.lib) gitignoreSource;
+      in {
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          pname = "nixos";
+          version = "0.1.0";
+          src = gitignoreSource ./.;
+
+          nativeBuildInputs = [zig];
+
+          dontConfigure = true;
+          dontInstall = true;
+
+          buildPhase = ''
+            mkdir -p $out
+            mkdir -p .cache/{p,z,tmp}
+            zig build install \
+              --cache-dir $(pwd)/zig-cache \
+              --global-cache-dir $(pwd)/.cache \
+              -Dcpu=baseline \
+              -Doptimize=ReleaseSafe \
+              --prefix $out
+          '';
+
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/water-sucks/nixos";
+            description = "A unified NixOS tooling replacement for nixos-* utilities";
+            license = licenses.gpl3Only;
+            maintainers = with maintainers; [water-sucks];
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          name = "nixos-shell";
+          packages = [
+            pkgs.alejandra
+            pkgs.zls
+          ];
+          nativeBuildInputs = [
+            zig
+          ];
+
+          ZIG_DOCS = "${zig}/doc/langref.html";
+          ZIG_STD_DOCS = "${zig}/doc/std/index.html";
+        };
+      };
+    };
+}
