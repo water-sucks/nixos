75a191bdaa17e95ecda35cd571446837a4ec82e2
Varun Narravula <varun@snare.dev>
2024-06-06 17:02:58 -0700

    refactor(flake): call fetchZigDeps in a simpler way

--- package.nix
+++ package.nix
@@ -1,8 +1,15 @@
-let
+{
+  stdenv,
+  lib,
+  zig,
+  nix-gitignore,
+  pkg-config,
+  autoPatchelfHook,
+  nix,
+  revision ? "dirty",
+  flake ? true,
+}: let
   fetchZigDeps = {
-    stdenv,
-    zig,
-  }: {
     name,
     src,
     packageRoot ? "./",
@@ -43,57 +50,46 @@ let
       outputHash = depsHash;
     };
 in
-  {
-    stdenv,
-    lib,
-    zig,
-    nix-gitignore,
-    pkg-config,
-    autoPatchelfHook,
-    nix,
-    revision ? "dirty",
-    flake ? true,
-  }:
-    stdenv.mkDerivation rec {
-      pname = "nixos";
-      version = "0.7.0-dev";
-      src = nix-gitignore.gitignoreSource [] ./.;
+  stdenv.mkDerivation rec {
+    pname = "nixos";
+    version = "0.7.0-dev";
+    src = nix-gitignore.gitignoreSource [] ./.;
 
-      postPatch = let
-        deps = fetchZigDeps {inherit stdenv zig;} {
-          name = pname;
-          src = ./.;
-          depsHash = "sha256-AtdKKvLYR865JRKKgNwiejo5kKEO88vg1N6GXdBBIsk=";
-        };
-      in ''
-        mkdir -p .cache
-        ln -s ${deps} .cache/p
-      '';
-
-      nativeBuildInputs = [zig pkg-config autoPatchelfHook];
-
-      buildInputs = [nix.dev];
-
-      dontConfigure = true;
-      dontInstall = true;
-
-      _NIXOS_GIT_REV = revision;
-
-      buildPhase = ''
-        mkdir -p $out
-        zig build install \
-          --cache-dir $(pwd)/zig-cache \
-          --global-cache-dir $(pwd)/.cache \
-          -Dcpu=baseline \
-          -Doptimize=ReleaseSafe \
-          -Dflake=${lib.boolToString flake} \
-          --prefix $out
-      '';
-
-      meta = with lib; {
-        homepage = "https://github.com/water-sucks/nixos";
-        description = "A unified NixOS tooling replacement for nixos-* utilities";
-        license = licenses.gpl3Only;
-        maintainers = with maintainers; [water-sucks];
+    postPatch = let
+      deps = fetchZigDeps {
+        name = pname;
+        src = ./.;
+        depsHash = "sha256-AtdKKvLYR865JRKKgNwiejo5kKEO88vg1N6GXdBBIsk=";
       };
-    }
+    in ''
+      mkdir -p .cache
+      ln -s ${deps} .cache/p
+    '';
+
+    nativeBuildInputs = [zig pkg-config autoPatchelfHook];
+
+    buildInputs = [nix.dev];
+
+    dontConfigure = true;
+    dontInstall = true;
+
+    _NIXOS_GIT_REV = revision;
+
+    buildPhase = ''
+      mkdir -p $out
+      zig build install \
+        --cache-dir $(pwd)/zig-cache \
+        --global-cache-dir $(pwd)/.cache \
+        -Dcpu=baseline \
+        -Doptimize=ReleaseSafe \
+        -Dflake=${lib.boolToString flake} \
+        --prefix $out
+    '';
+
+    meta = with lib; {
+      homepage = "https://github.com/water-sucks/nixos";
+      description = "A unified NixOS tooling replacement for nixos-* utilities";
+      license = licenses.gpl3Only;
+      maintainers = with maintainers; [water-sucks];
+    };
+  }
