32941f975e03956567c84ddd6435e165a294a3bc
Varun Narravula <varun@snare.dev>
2024-07-23 22:44:44 -0700

    fix(config): do not fallback to defaults if bad values are detected

--- src/config.zig
+++ src/config.zig
@@ -3,6 +3,7 @@ const mem = std.mem;
 const json = std.json;
 const posix = std.posix;
 const Allocator = mem.Allocator;
+const ArrayList = std.ArrayList;
 
 const Constants = @import("constants.zig");
 
@@ -11,6 +12,7 @@ const log = @import("log.zig");
 const toml = @import("toml");
 
 const utils = @import("utils.zig");
+const ansi = utils.ansi;
 const fileExistsAbsolute = utils.fileExistsAbsolute;
 const readFile = utils.readFile;
 
@@ -33,10 +35,6 @@ pub const Config = struct {
     } = .{},
 };
 
-pub const ParseConfigError = error{
-    InvalidValue,
-};
-
 var config_value: ?toml.Parsed(Config) = null;
 
 pub fn getConfig() Config {
@@ -47,10 +45,7 @@ pub fn parseConfig(allocator: Allocator) !void {
     const config_location = posix.getenv("NIXOS_CLI_CONFIG") orelse Constants.default_config_location;
 
     const config_str = readFile(allocator, config_location) catch |err| {
-        switch (err) {
-            error.FileNotFound => return,
-            else => log.err("error opening config: {s}", .{@errorName(err)}),
-        }
+        log.err("error opening config: {s}", .{@errorName(err)});
         return err;
     };
     defer allocator.free(config_str);
@@ -63,53 +58,67 @@ pub fn parseConfig(allocator: Allocator) !void {
         return err;
     };
     errdefer deinit();
-    const config = parsed.value;
+    var config = parsed.value;
 
     // Validation
-    if (config.aliases) |aliases| {
+    if (config.aliases) |*aliases| {
+        var values_to_remove = ArrayList([]const u8).init(allocator);
+        defer values_to_remove.deinit();
+
         var it = aliases.iterator();
+
         while (it.next()) |kv| {
             const key = kv.key_ptr.*;
             const value = kv.value_ptr.*;
 
             if (key.len == 0) {
                 configError("aliases: alias name cannot be empty", .{});
-                return ParseConfigError.InvalidValue;
-            }
-            if (mem.startsWith(u8, key, "-")) {
+                try values_to_remove.append(key);
+            } else if (mem.startsWith(u8, key, "-")) {
                 configError("aliases: alias '{s}' cannot start with a '-'", .{key});
-                return ParseConfigError.InvalidValue;
-            }
-            if (mem.indexOfAny(u8, key, &std.ascii.whitespace) != null) {
+                try values_to_remove.append(key);
+            } else if (mem.indexOfAny(u8, key, &std.ascii.whitespace) != null) {
                 configError("aliases: alias '{s}' cannot have whitespace", .{key});
-                return ParseConfigError.InvalidValue;
-            }
-            if (std.meta.activeTag(value) != .array) {
+                try values_to_remove.append(key);
+            } else if (std.meta.activeTag(value) != .array) {
                 configError("aliases.{s}: expected type array, got type {s}", .{ key, @tagName(value) });
-                return ParseConfigError.InvalidValue;
-            }
-            if (value.array.items.len == 0) {
+                try values_to_remove.append(key);
+            } else if (value.array.items.len == 0) {
                 configError("aliases.{s}: args list cannot be empty", .{key});
-                return ParseConfigError.InvalidValue;
-            }
-            for (value.array.items) |arg| {
-                if (std.meta.activeTag(arg) != .string) {
-                    configError("aliases.{s}: expected type string for args array, got type {s}", .{ key, @tagName(arg) });
-                    return ParseConfigError.InvalidValue;
+                try values_to_remove.append(key);
+            } else {
+                for (value.array.items) |arg| {
+                    if (std.meta.activeTag(arg) != .string) {
+                        configError("aliases.{s}: expected type string for args array, got type {s}", .{ key, @tagName(arg) });
+                        try values_to_remove.append(key);
+                        break;
+                    }
                 }
             }
         }
+
+        for (values_to_remove.items) |key| {
+            _ = aliases.remove(key);
+        }
     }
 
-    if (config.init.extra_attrs) |extra_attrs| {
+    if (config.init.extra_attrs) |*extra_attrs| {
+        var values_to_remove = ArrayList([]const u8).init(allocator);
+        defer values_to_remove.deinit();
+
         var it = extra_attrs.iterator();
         while (it.next()) |kv| {
             const key = kv.key_ptr.*;
             const value = kv.value_ptr.*;
+
             if (std.meta.activeTag(value) != .string) {
                 configError("init.extra_attrs: expected type string for key '{s}', got type {s}", .{ key, @tagName(value) });
-                return ParseConfigError.InvalidValue;
+                try values_to_remove.append(key);
             }
+        }
+
+        for (values_to_remove.items) |key| {
+            _ = extra_attrs.remove(key);
         }
     }
 
@@ -124,7 +133,11 @@ pub fn deinit() void {
 }
 
 fn configError(comptime fmt: []const u8, args: anytype) void {
-    log.print("error: invalid setting: ", .{});
+    if (Constants.use_color) {
+        log.print(ansi.BOLD ++ ansi.RED ++ "error" ++ ansi.RESET ++ ": ", .{});
+    } else {
+        log.print("error: ", .{});
+    }
+    log.print("invalid setting: ", .{});
     log.print(fmt ++ "\n", args);
-    log.print("\nFor more information, run `nixos --help`.\n", .{});
 }
