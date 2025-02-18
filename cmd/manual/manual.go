762238da8df72f181816749b1c88bbd36d5cd795
Varun Narravula <varun@snare.dev>
2025-02-18 11:32:31 -0800

    feat(manual): implement manual cmd

--- cmd/manual/manual.go
+++ cmd/manual/manual.go
@@ -1,9 +1,16 @@
 package manual
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
 	"github.com/spf13/cobra"
 	cmdUtils "github.com/water-sucks/nixos/internal/cmd/utils"
+	"github.com/water-sucks/nixos/internal/constants"
 	"github.com/water-sucks/nixos/internal/logger"
+	"github.com/water-sucks/nixos/internal/system"
 )
 
 func ManualCommand() *cobra.Command {
@@ -22,9 +29,58 @@ func ManualCommand() *cobra.Command {
 	return &cmd
 }
 
+const (
+	localManualFile = constants.CurrentSystem + "/sw/share/doc/nixos/index.html"
+	manualURL       = "https://nixos.org/manual/nixos/stable"
+)
+
 func manualMain(cmd *cobra.Command) error {
 	log := logger.FromContext(cmd.Context())
+	s := system.NewLocalSystem()
 
-	log.Info("manual")
+	if !s.IsNixOS() {
+		log.Error("this command is only supported on NixOS systems")
+		return nil
+	}
+
+	url := localManualFile
+	if _, err := os.Stat(url); err != nil {
+		log.Error("local documentation is not available, opening manual for current NixOS stable version")
+		url = manualURL
+	}
+
+	var openCommand string
+
+	browsers := strings.Split(os.Getenv("BROWSERS"), ":")
+	for _, browser := range browsers {
+		if p, err := exec.LookPath(browser); err == nil && p != "" {
+			openCommand = p
+			break
+		}
+	}
+
+	defaultCommands := []string{"xdg-open", "w3m", "open"}
+	if openCommand == "" {
+		for _, cmd := range defaultCommands {
+			if p, err := exec.LookPath(cmd); err == nil && p != "" {
+				openCommand = p
+				break
+			}
+		}
+
+		if openCommand == "" {
+			msg := "unable to locate suitable browser to open manual, exiting"
+			log.Error(msg)
+			return fmt.Errorf("%v", msg)
+		}
+	}
+
+	log.Infof("opening manual using %v", openCommand)
+	err := exec.Command(openCommand, url).Run()
+	if err != nil {
+		log.Errorf("failed to open manual: %v", err)
+		return err
+	}
+
 	return nil
 }
